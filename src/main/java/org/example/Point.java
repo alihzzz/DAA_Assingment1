92f7be2f555d64a83ecd7d432ecc5304264e69e2
Alikhan Smagul <254457@astanait.edu.kz>
2026-09-20 20:49:00 +0500

    created point class with closest pair

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
